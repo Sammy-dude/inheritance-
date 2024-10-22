1bd0b91eb1977ba146086f034385e710e54e0581
ousamasaloum <osaloum50@gmail.com>
2024-10-22 11:06:31 -0400

    this is my first change

--- src/main/java/com/javatechie/solid/isp/GooglePay.java
+++ src/main/java/com/javatechie/solid/isp/GooglePay.java
@@ -1,5 +1,7 @@
 package com.javatechie.solid.isp;
 
+import java.util.Date;
+
 public class GooglePay implements UPIPayments,CashBackManager {
 
     public void payMoney() {
@@ -11,6 +13,19 @@ public class GooglePay implements UPIPayments,CashBackManager {
     }
 
     public void getCashBackAsCreditBalance() {
+        System.out.println("we are inside the getCashBackAsCreditBalance, our current date is  " + java.time.LocalDate.now());
       //this features is there in gpay
+    }
+    public static void getCashBackAsCreditBalanceS() {
+        System.out.println("we are inside the Static getCashBackAsCreditBalance, our current date is  " + java.time.LocalDate.now());
+        //this features is there in gpay
+    }
+    public static void main(String[] args) {
+        GooglePay googlePay = new GooglePay();
+        googlePay.getScratchCard();
+        googlePay.getCashBackAsCreditBalance();
+        GooglePay.getCashBackAsCreditBalanceS();
+
+
     }
 }
